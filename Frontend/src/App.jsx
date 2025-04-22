4068961ccfec702d38dd0e10af2fd39a72846bfc
Mahiimaa <137261863+Mahiimaa@users.noreply.github.com>
2025-04-22 08:31:41 +0545

    initialized project

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Register from "./pages/Register";
+import './App.css'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+      <Route path="/" element ={<Register/>}/>
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
---
+++ Frontend/src/pages/Register.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Register() {
+  return (
+    <div className="bg-red-950">Register</div>
+  )
+}
+
+export default Register
