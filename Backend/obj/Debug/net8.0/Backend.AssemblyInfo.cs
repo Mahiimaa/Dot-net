518f89a5f7f5714a05b964b812c4d2c274d958a8
Nabi-Shrestha <np05cp4a220091@iic.edu.np>
2025-05-02 22:15:08 +0545

    Profile home

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1628ef482ba3178f67ff815eb9e53173beaee90e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a2086c34a4d822ecb38078ca22323542c821bad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
